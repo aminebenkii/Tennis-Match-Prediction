b0bf07c9ef4cc34f4d461985cbb98d92c9adb182
aminebenkii <aminebenkirane.pro@gmail.com>
2024-12-26 17:42:11 +0100

    Update

--- Preprocessed_data/Preprocess_data.py
+++ Preprocessed_data/Preprocess_data.py
@@ -1,6 +1,8 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import os
+
+
 
 
 def read_files_to_dataframe(directory, years):
@@ -23,9 +25,43 @@ def read_files_to_dataframe(directory, years):
     return combined_df
 
 
+def clip_rename_and_flip_dataframe(historical_data_df):
+    
+    historical_data_df = historical_data_df[['Date', 'Surface', 'Winner', 'Loser', 'WRank', 'LRank', 'WPts', 'LPts', 'W1', 'L1', 'W2', 'L2', 'W3', 'L3', 'W4', 'L4', 'W5', 'L5', 'AvgW', 'AvgL']]
+
+    df = historical_data_df.rename(columns={
+        'W1': 'P1S1', 'L1': 'P2S1',
+        'W2': 'P1S2', 'L2': 'P2S2',
+        'W3': 'P1S3', 'L3': 'P2S3',
+        'W4': 'P1S4', 'L4': 'P2S4',
+        'W5': 'P1S5', 'L5': 'P2S5',
+        'Winner': 'P1', 'Loser': 'P2',
+        'WRank': 'P1_Rank', 'LRank': 'P2_Rank',
+        'WPts': 'P1_ATPts', 'LPts': 'P2_ATPts',
+        'AvgW': 'P1_Avg_Odds', 'AvgL': 'P2_Avg_Odds'
+    })
+
+    # Flip rows for alternating matches nad have a 50% Baseline
+    df.loc[1::2, ['P1', 'P2']] = df.loc[1::2, ['P2', 'P1']].values
+    df.loc[1::2, ['P1_Rank', 'P2_Rank']] = historical_data_df.loc[1::2, ['LRank', 'WRank']].values
+    df.loc[1::2, ['P1_ATPts', 'P2_ATPts']] = historical_data_df.loc[1::2, ['LPts', 'WPts']].values
+    df.loc[1::2, ['P1S1', 'P2S1']] = historical_data_df.loc[1::2, ['L1', 'W1']].values
+    df.loc[1::2, ['P1S2', 'P2S2']] = historical_data_df.loc[1::2, ['L2', 'W2']].values
+    df.loc[1::2, ['P1S3', 'P2S3']] = historical_data_df.loc[1::2, ['L3', 'W3']].values
+    df.loc[1::2, ['P1S4', 'P2S4']] = historical_data_df.loc[1::2, ['L4', 'W4']].values
+    df.loc[1::2, ['P1S5', 'P2S5']] = historical_data_df.loc[1::2, ['L5', 'W5']].values
+    df.loc[1::2, ['P1_Avg_Odds', 'P2_Avg_Odds']] = historical_data_df.loc[1::2, ['AvgL', 'AvgW']].values
+
+    # Create a Winner column
+    df['Winner'] = (df['P1'] != historical_data_df['Winner']).astype(int) + 1
+
+    df[['P1S1', 'P2S1', 'P1S2', 'P2S2', 'P1S3', 'P2S3', 'P1S4', 'P2S4', 'P1S5', 'P2S5']] = df[['P1S1', 'P2S1', 'P1S2', 'P2S2', 'P1S3', 'P2S3', 'P1S4', 'P2S4', 'P1S5', 'P2S5']].fillna(0)
+
+    return df
+
+
 def initialize_dictionaries(unique_players):
     
-
     total_points_diff = {player: 0 for player in unique_players}
     sets_played = {player: 0 for player in unique_players}
     matches_played = {player: 0 for player in unique_players}
@@ -46,11 +82,13 @@ def initialize_dictionaries(unique_players):
 
 
 def update_dictionaries(row, total_points_diff, sets_played, matches_played, win_record, h2h_records, surface_stats):
-   
+    
     p1, p2 = row['P1'], row['P2']
-    winner = row['Winner']
+    p1_rank, p2_rank = row['P1_Rank'], row['P2_Rank']
     p1_ATPts, p2_ATPts = row['P1_ATPts'], row['P2_ATPts']
     surface = row['Surface']
+    winner = row['Winner']
+
 
     matches_played[p1] += 1
     matches_played[p2] += 1
@@ -66,7 +104,7 @@ def update_dictionaries(row, total_points_diff, sets_played, matches_played, win
     sets_played[p1] += match_sets_played
     sets_played[p2] += match_sets_played
 
-    if abs(p1_ATPts - p2_ATPts) <= 500:
+    if abs(p1_rank - p2_rank) <= 100:
         win_record[p1].append(1 if winner == 1 else 0)
         win_record[p2].append(1 if winner == 2 else 0)
 
@@ -79,7 +117,7 @@ def update_dictionaries(row, total_points_diff, sets_played, matches_played, win
         h2h_records[(p1, p2)]['outcomes'].append(0)
         h2h_records[(p2, p1)]['outcomes'].append(1)
 
-    if abs(p1_ATPts - p2_ATPts) <= 500:
+    if abs(p1_rank - p2_rank) <= 100:
         surface_stats[p1][surface]['games'] += 1
         surface_stats[p2][surface]['games'] += 1
         if winner == 1:
@@ -89,28 +127,33 @@ def update_dictionaries(row, total_points_diff, sets_played, matches_played, win
 
 
 def calculate_transformed_row(row, total_points_diff, sets_played, matches_played, win_record, h2h_records, surface_stats):
-    
 
     p1, p2 = row['P1'], row['P2']
+    p1_rank, p2_rank = row['P1_Rank'], row['P2_Rank']
     p1_atpts, p2_atpts = row['P1_ATPts'], row['P2_ATPts']
     surface = row['Surface']
 
-    p1_avg = total_points_diff[p1] / sets_played[p1] if sets_played[p1] != 0 else 0
-    p2_avg = total_points_diff[p2] / sets_played[p2] if sets_played[p2] != 0 else 0
+    p1_avg = total_points_diff[p1] / sets_played[p1] if sets_played[p1] != 0 else np.nan
+    p2_avg = total_points_diff[p2] / sets_played[p2] if sets_played[p2] != 0 else np.nan
 
-    p1_wr = sum(win_record[p1][-10:]) / len(win_record[p1][-10:]) if len(win_record[p1][-10:]) > 0 else 0
-    p2_wr = sum(win_record[p2][-10:]) / len(win_record[p2][-10:]) if len(win_record[p2][-10:]) > 0 else 0
+    p1_wr = sum(win_record[p1][-10:]) / len(win_record[p1][-10:]) if len(win_record[p1][-10:]) > 0 else np.nan
+    p2_wr = sum(win_record[p2][-10:]) / len(win_record[p2][-10:]) if len(win_record[p2][-10:]) > 0 else np.nan
 
     h2h_matches = min(10, h2h_records[(p1, p2)]['matches'])
     h2h_outcomes = h2h_records[(p1, p2)]['outcomes'][-h2h_matches:]
-    h2h_p1_p2 = sum(h2h_outcomes) / h2h_matches if h2h_matches > 0 else 0
+    h2h_p1_p2 = sum(h2h_outcomes) / h2h_matches if h2h_matches > 0 else np.nan
 
-    surface_wr_p1 = surface_stats[p1][surface]['wins'] / surface_stats[p1][surface]['games'] if surface_stats[p1][surface]['games'] > 0 else 0
-    surface_wr_p2 = surface_stats[p2][surface]['wins'] / surface_stats[p2][surface]['games'] if surface_stats[p2][surface]['games'] > 0 else 0
+    surface_wr_p1 = surface_stats[p1][surface]['wins'] / surface_stats[p1][surface]['games'] if surface_stats[p1][surface]['games'] > 0 else np.nan
+    surface_wr_p2 = surface_stats[p2][surface]['wins'] / surface_stats[p2][surface]['games'] if surface_stats[p2][surface]['games'] > 0 else np.nan
 
     return {
+        'Date' : row['Date'],
         'P1': p1,
         'P2': p2,
+        'P1_Rank': row['P1_Rank'],
+        'P2_Rank': row['P2_Rank'],
+        'P1_ATPts': p1_atpts,
+        'P2_ATPts': p2_atpts,
         'DIFF_ATPts': p1_atpts - p2_atpts,
         'P1_Avg': round(p1_avg, 2),
         'P2_Avg': round(p2_avg, 2),
@@ -134,7 +177,6 @@ def calculate_transformed_row(row, total_points_diff, sets_played, matches_playe
 
 def preprocess_data(df, split_index):
     
-    df = df[['Date', 'Surface', 'Winner', 'Loser', 'WPts', 'LPts', 'W1', 'L1', 'W2', 'L2', 'W3', 'L3','W4','L4','W5','L5', 'AvgW', 'AvgL']]
     unique_players = pd.concat([df['P1'], df['P2']]).unique()
     total_points_diff, sets_played, matches_played, win_record, h2h_records, surface_stats = initialize_dictionaries(unique_players)
 
@@ -152,12 +194,12 @@ def preprocess_data(df, split_index):
     return pd.DataFrame(new_df)
 
 
-
 data_dir = "../Raw_historical_data"
-file_years = range(2023, 2025)  
+file_years = range(2020, 2025)
 
 historical_data_df = read_files_to_dataframe(data_dir, file_years)
+df = clip_rename_and_flip_dataframe(historical_data_df)
 
-new_df = preprocess_data(historical_data_df, split_index=5000)
 
-new_df.to_csv('transformed_data.csv', index=False)
+new_df = preprocess_data(df, split_index=int(0.5*len(df)))
+new_df.to_csv('Preprocessed_data.csv', index=False)
